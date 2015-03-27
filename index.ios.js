2ed001373ed4e1e7fadf7766c9866d353d57b640
Kamil Przeorski <kamil@przeorski.pl>
2015-03-27 20:01:11 +0100

    valid api.rottentomatoes.com key + fetching data

--- index.ios.js
+++ index.ios.js
@@ -8,6 +8,14 @@ var React = require('react-native');
 var { AppRegistry, Image, StyleSheet, Text, View, } = React;
 
 
+var API_KEY = 'wgcxhbxsrzrmjnzmh4d3hnbg'; 
+var API_URL = 'http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json'; 
+var PAGE_SIZE = 25; 
+var PARAMS = '?apikey=' + API_KEY + '&page_limit=' + PAGE_SIZE; 
+var REQUEST_URL = API_URL + PARAMS;
+
+
+
 var MOCKED_MOVIES_DATA = [ 
   {
     title: 'Title', 
@@ -20,14 +28,54 @@ var MOCKED_MOVIES_DATA = [
 
 
 var AwesomeProject = React.createClass({
-  render: function() {
-    var movie = MOCKED_MOVIES_DATA[0];
+  getInitialState: function() { 
+    return { movies: null, }; 
+  },
+  componentDidMount: function() { 
+    this.fetchData(); 
+  },
+  fetchData: function() { 
+    fetch(REQUEST_URL) 
+    .then((response) => response.json()) 
+    .then((responseData) => { 
+      this.setState({ 
+        movies: responseData.movies, 
+        error: responseData.error, 
+      }); 
+    }) 
+    .done(); 
+  },
+  render: function() { 
+    console.log(this.state.movies);
+    if(this.state.error) {
+      return this.renderErrorView();
+    } else if (!this.state.movies) { 
+      return this.renderLoadingView(); 
+    }
+    var movie = this.state.movies[0]; 
+    return this.renderMovie(movie); 
+  },
+  renderLoadingView: function() { 
+    return ( 
+      <View style={styles.container}> 
+      <Text> Loading movies... </Text> 
+      </View> 
+      ); 
+  },
+  renderErrorView: function() { 
+    return ( 
+      <View style={styles.container}> 
+      <Text> Error: {this.state.error} </Text> 
+      </View> 
+      ); 
+  },
+  renderMovie: function(movie) {
     return (
       <View style={styles.container}>
         <Image source={{uri: movie.posters.thumbnail}} style={styles.thumbnail} />
         <View style={styles.rightContainer}>
-          <Text>{movie.title}</Text>
-          <Text>{movie.year}</Text>
+          <Text style={styles.title}>{movie.title}</Text>
+          <Text style={styles.year}>{movie.year}</Text>
         </View>
       </View>
     );
@@ -44,7 +92,6 @@ var styles = StyleSheet.create({
   },
   rightContainer: { 
     flex: 1, 
-    backgroundColor: 'red',
   },
   thumbnail: { 
     width: 53, 
